11910498991c0c586eb5ad279901f91adee17725
Peally <pieclandre@gmail.com>
2023-06-23 23:08:49 +0200

    feat : get all html from design

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,54 @@
+import Footer from "../Components/Footer";
+
+const Login = () => {
+  return (
+    <>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src="./img/argentBankLogo.png"
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="./sign-in.html">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </a>
+        </div>
+      </nav>
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
+            <a href="./user.html" className="sign-in-button">
+              Sign In
+            </a>
+            {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
+            <button className="sign-in-button">Sign In</button>
+            {/* <!--  --> */}
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
