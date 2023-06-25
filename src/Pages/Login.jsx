054c3067216687da5d8b1cae75e0b9672b04b023
Peally <pieclandre@gmail.com>
2023-06-25 20:11:13 +0200

    update : import component Header

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,24 +1,10 @@
 import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 
 const Login = () => {
   return (
     <>
-      <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
-          <img
-            className="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </a>
-        <div>
-          <a className="main-nav-item" href="./sign-in.html">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </a>
-        </div>
-      </nav>
+      <Header />
       <main className="main bg-dark">
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
