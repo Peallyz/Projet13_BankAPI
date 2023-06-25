69e9cf2f2888e88abc5bcf60d304d6c26ae9bdfe
Peally <pieclandre@gmail.com>
2023-06-25 20:10:17 +0200

    feat : create header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,20 @@
+import { NavLink } from "react-router-dom";
+import Login from "./Login";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <nav className="main-nav">
+      <NavLink className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src="./img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </NavLink>
+      <Login />
+    </nav>
+  );
 };
 
 export default Header;
