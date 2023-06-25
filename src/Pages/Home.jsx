37c07cdb3062f31f9f0eb21e0a10b09dfaac5317
Peally <pieclandre@gmail.com>
2023-06-25 20:11:07 +0200

    update : import component Header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,10 @@
-import { NavLink } from "react-router-dom";
 import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 
 const Home = () => {
   return (
     <>
-      <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
-          <img
-            className="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </a>
-        <div>
-          <NavLink to="/login" className="main-nav-item">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </NavLink>
-        </div>
-      </nav>
+      <Header />
       <main>
         <div className="hero">
           <section className="hero-content">
