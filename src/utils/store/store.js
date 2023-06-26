d38c83d3a925a7f00659795b38085a4760bc344f
Peally <pieclandre@gmail.com>
2023-06-26 22:55:37 +0200

    refacto : rename slice folder

--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../user/user.slice";
+import userReducer from "../slice/user.slice";
 
 export default configureStore({
   reducer: {
