b55d552c25a8a1a4c11761715a1938007aa3deca
Peally <pieclandre@gmail.com>
2023-06-27 21:23:15 +0200

    feat : create an account

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,4 +1,38 @@
+import { PropTypes } from "prop-types";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import signUp from "../utils/asyncThunk/signUp";
+
 const SignUp = ({ setIsSignInModal }) => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const responseStatus = useSelector((state) => state.user.signUpStatus);
+
+  useEffect(() => {
+    if (responseStatus === 200) {
+      setIsSignInModal(true);
+    }
+
+    if (responseStatus && responseStatus !== 200) {
+      alert("Invalid form");
+    }
+  }, [responseStatus]);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      signUp({
+        email: usernameRef.current.value,
+        password: passwordRef.current.value,
+      })
+    );
+  };
   return (
     <section className="sign-in-content">
       <i className="fa fa-user-circle sign-in-icon"></i>
@@ -6,21 +40,24 @@ const SignUp = ({ setIsSignInModal }) => {
       <form>
         <div className="input-wrapper">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input type="text" id="username" ref={usernameRef} required />
         </div>
         <div className="input-wrapper">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" ref={passwordRef} required />
         </div>
         <div className="input-wrapper">
           <label htmlFor="firstname">Firstname</label>
-          <input type="firstname" id="firstname" />
+          <input type="firstname" id="firstname" ref={firstNameRef} required />
         </div>
         <div className="input-wrapper">
           <label htmlFor="lastname">Lastname</label>
-          <input type="lastname" id="lastname" />
+          <input type="lastname" id="lastname" ref={lastNameRef} required />
         </div>
 
+        <button className="sign-in-button" onClick={handleSubmit}>
+          Sign Up
+        </button>
         <button
           className="sign-in-button"
           onClick={(e) => {
@@ -28,13 +65,15 @@ const SignUp = ({ setIsSignInModal }) => {
             setIsSignInModal(true);
           }}
         >
-          Sign In
+          Already have an account?
         </button>
-
-        <button className="sign-in-button">Sign Up</button>
       </form>
     </section>
   );
+};
+
+SignUp.propTypes = {
+  setIsSignInModal: PropTypes.func.isRequired,
 };
 
 export default SignUp;
