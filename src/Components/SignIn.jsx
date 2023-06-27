563cee6a654ae0a9ebdd626a73e31b6e12fda6be
Peally <pieclandre@gmail.com>
2023-06-27 21:23:07 +0200

    feat : login with an account

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,4 +1,40 @@
+import { PropTypes } from "prop-types";
+import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import login from "../utils/asyncThunk/login";
+
 const SignIn = ({ setIsSignInModal }) => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const responseStatus = useSelector((state) => state.user.signInStatus);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (responseStatus === 200) {
+      navigate("/home");
+    }
+
+    if (responseStatus && responseStatus !== 200) {
+      alert("Invalid username or password");
+    }
+  }, [responseStatus, navigate]);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      login({
+        email: usernameRef.current.value,
+        password: passwordRef.current.value,
+      })
+    );
+  };
+
   return (
     <section className="sign-in-content">
       <i className="fa fa-user-circle sign-in-icon"></i>
@@ -6,30 +42,36 @@ const SignIn = ({ setIsSignInModal }) => {
       <form>
         <div className="input-wrapper">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input type="text" id="username" ref={usernameRef} required />
         </div>
         <div className="input-wrapper">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" ref={passwordRef} required />
         </div>
         <div className="input-remember">
           <input type="checkbox" id="remember-me" />
           <label htmlFor="remember-me">Remember me</label>
         </div>
+        <button className="sign-in-button" onClick={handleSubmit}>
+          Sign In
+        </button>
         <button
+          role="button"
           className="sign-in-button"
           onClick={(e) => {
             e.preventDefault;
             setIsSignInModal(false);
           }}
         >
-          Sign Up
+          Create an account
         </button>
-
-        <button className="sign-in-button">Sign In</button>
       </form>
     </section>
   );
+};
+
+SignIn.propTypes = {
+  setIsSignInModal: PropTypes.func.isRequired,
 };
 
 export default SignIn;
