d5325c183dc36be100336fb5b76ffe776ce5e647
Peally <pieclandre@gmail.com>
2023-06-28 23:43:01 +0200

    feat : handle getProfile API call and display dynamic data

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,6 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../Components/Header";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import getProfile from "../utils/asyncActions/getProfile";
+import Footer from "../Components/Footer";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const userToken = useSelector((state) => state.user.token);
+  const firstname = useSelector((state) => state.user.firstname);
+  const lastname = useSelector((state) => state.user.lastname);
+
+  useEffect(() => {
+    if (!userToken) {
+      navigate("/login");
+    }
+    if (!firstname || !lastname) {
+      dispatch(getProfile(userToken));
+    }
+  }, [firstname, lastname, userToken]);
   return (
     <>
       <Header />
@@ -9,42 +30,13 @@ const Dashboard = () => {
           <h1>
             Welcome back
             <br />
-            Tony Jarvis!
+            {`${firstname} ${lastname}`}!
           </h1>
           <button className="edit-button">Edit Name</button>
         </div>
         <h2 className="sr-only">Accounts</h2>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
       </main>
+      <Footer />
     </>
   );
 };
