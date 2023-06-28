6d5c6bddd7c1dcdde8fe3e9288d00597ef9289af
Peally <pieclandre@gmail.com>
2023-06-28 23:45:14 +0200

    feat : reducers to login, signOut, create account and get needed data on store

--- src/utils/slice/user.slice.js
+++ src/utils/slice/user.slice.js
@@ -1,23 +1,64 @@
 import { createSlice } from "@reduxjs/toolkit";
+import login from "../asyncActions/login";
+import signUp from "../asyncActions/signUp";
+import getProfile from "../asyncActions/getProfile";
 
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    userData: null,
+    username: null,
+    firstname: null,
+    lastname: null,
     token: null,
+    signInStatus: null,
+    signUpStatus: null,
+    getProfileStatus: null,
   },
   reducers: {
-    setData: (state, { payload }) => {
-      state.userData = payload;
+    signOut: (state) => {
+      state.username = null;
+      state.firstname = null;
+      state.lastname = null;
+      state.token = null;
+      state.signInStatus = null;
+      state.signUpStatus = null;
+      state.getProfileStatus = null;
     },
-    setUpdateData: (state, { payload }) => {
-      state.userData = [...state.userData, payload];
+    resetSignUpStatus: (state) => {
+      state.signUpStatus = null;
     },
-    setToken: (state, { payload }) => {
-      state.token = payload;
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, { payload }) => {
+        state.username = payload.payload.email;
+        state.token = payload?.data?.body?.token;
+        state.signInStatus = payload.data.status;
+      })
+      .addCase(login.rejected, (state) => {
+        state.username = null;
+        state.token = null;
+        state.signInStatus = 500;
+      })
+      .addCase(signUp.fulfilled, (state, { payload }) => {
+        state.username = payload.payload.email;
+        state.signUpStatus = payload.data.status;
+      })
+      .addCase(signUp.rejected, (state) => {
+        state.username = null;
+        state.signUpStatus = 500;
+      })
+      .addCase(getProfile.fulfilled, (state, { payload }) => {
+        state.firstname = payload?.body?.firstName;
+        state.lastname = payload?.body?.lastName;
+        state.getProfileStatus = payload?.data?.status;
+      })
+      .addCase(getProfile.rejected, (state) => {
+        state.username = null;
+        state.getProfile = 500;
+      });
   },
 });
 
-export const { setData, setUpdateData, setToken } = userSlice.actions;
+export const { signOut } = userSlice.actions;
 export default userSlice.reducer;
