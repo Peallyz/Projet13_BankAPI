3ed95ac30eb828641d537bf72eeed6e35ca4f0a2
Peally <pieclandre@gmail.com>
2023-06-30 11:12:48 +0200

    Feat : create 404 error pages

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,20 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
 const Error = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>404</h1>
+          <h2>Page not found</h2>
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Error;
